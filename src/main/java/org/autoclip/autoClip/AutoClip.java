b1aaaa48aed6710cb585005e4b7e810b367e5e21
Mitchell Stahl <mbs22f@fsu.edu>
2024-11-09 03:29:30 -0500

    initial

---
+++ src/main/java/org/autoclip/autoClip/AutoClip.java
@@ -0,0 +1,17 @@
+package org.autoclip.autoClip;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoClip extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.print("hello");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
